915b843469d1b9ad009def8d5535d8f08b7915a1
Jacobo <a@a.com>
2021-01-20 21:28:20 +0100

    Create helm and update scripts.

--- build-images.ps1
+++ build-images.ps1
@@ -1,3 +1,4 @@
-Set-Location images/base
+Set-Location (Join-Path -Path $PSScriptRoot -ChildPath "images\base")
 
-docker build -t azure-pipelines-agent:latest
+docker build -t docker-registry.cloud.com/azure-pipelines-agent:latest .
+docker push docker-registry.cloud.com/azure-pipelines-agent:latest
